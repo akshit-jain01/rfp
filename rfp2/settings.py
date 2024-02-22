1d87825cbe0d15fd12617e028216beea8d99a9b8
akshit-jain01 <akshitjain.1112003@gmail.com>
2024-02-22 15:45:20 +0530

    migrated to postgres

--- rfp2/settings.py
+++ rfp2/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'rfp2.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres.aajjlkwttefjiympdymk',
+        'PASSWORD': 'Ajain*112003',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 
